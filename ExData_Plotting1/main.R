0b081e13c657bb332aa4816013af2b37b3df6495
atet <atet.kao@uci.edu>
2014-07-11 14:16:16 -0700

    New file to organize plotting scripts

---
+++ ExData_Plotting1/main.R
@@ -0,0 +1,27 @@
+## AK 20140711
+## github: atet
+## NOTE: I am submitting this early, but seeing how this is a public repos, anyone can see it right away.
+## If you need help, you can use this as a guide to understanding the problem, but please do not just copy/paste this code.
+## R has a steep learning curve, but you will get it with time and experience, I have faith in you.
+#####################################################################################################
+# ~/git_coursera$ git clone https://github.com/rdpeng/ExData_Plotting1
+# ~/git_coursera$ git add ExData_Plotting1/
+# ~/git_coursera$ git commit -a -m "Forked https://github.com/rdpeng/ExData_Plotting1"
+# ~/git_coursera$ git push
+
+# Goal: Make plots, examine how household energy usage varies over a 2-day period in February, 2007.
+#   Your task is to reconstruct the following 4 plots below, all of which were constructed using
+#   the base plotting system.
+# Instuctions:
+# 1. We will only be using data from the dates 2007-02-01 and 2007-02-02.
+# 2. Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+# 3. Name each of the plot files as plot1.png, plot2.png, etc.
+# 4. Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot,
+#      i.e. code in plot1.R constructs the plot1.png plot. Your code file should include code
+#      for reading the data so that the plot can be fully reproduced. You should also include
+#      the code that creates the PNG file.
+# 5. Add the PNG file and R code file to your git repository
+
+
+
+

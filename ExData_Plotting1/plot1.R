dcd13b32fab37695dbcd53678469451ebad9ae40
atet <atet@users.noreply.github.com>
2014-07-12 15:36:28 -0700

    Uploading plot1.R and plot1.png

--- ExData_Plotting1/plot1.R
+++ ExData_Plotting1/plot1.R
@@ -1,23 +1,3 @@
-## AK 20140712
-## github: atet
-## NOTE: I am submitting this early, but seeing how this is a public repos, anyone can see it right away.
-## If you need help, you can use this as a guide to understanding the problem, but please do not just copy/paste this code.
-## R has a steep learning curve, but you will get it with time and experience, I have faith in you.
-#####################################################################################################
-# Goal: Make plots, examine how household energy usage varies over a 2-day period in February, 2007.
-#   Your task is to reconstruct the following 4 plots below, all of which were constructed using
-#   the base plotting system.
-# Instuctions:
-# 1. We will only be using data from the dates 2007-02-01 and 2007-02-02.
-# 2. Construct the plot (using base) and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
-# 3. Name each of the plot files as plot1.png, plot2.png, etc.
-# 4. Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot,
-#      i.e. code in plot1.R constructs the plot1.png plot. Your code file should include code
-#      for reading the data so that the plot can be fully reproduced. You should also include
-#      the code that creates the PNG file.
-# 5. Add the PNG file and R code file to your git repository
-
-
 #####################################################################################################
 ## AK 20140712
 ## github: atet
@@ -36,28 +16,11 @@
 #      the code that creates the PNG file.
 # 5. Add the PNG file and R code file to your git repository
 
-#########
-# plot1
-#########
+##################
+# plot1 code
+##################
 
-# Load data directly from online https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-library(RCurl)
-file <- CFILE("exdata%2Fdata%2Fhousehold_power_consumption.zip", mode="wb")
-data <- curlPerform(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", writedata = file@ref)
-close(file)
-str(data)
-
-
-
-library(RCurl)
-filepath <- getURL("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")
-file <- tempfile()
-con <- file(file, open = "w")
-cat(filepath, file = con) 
-close(con)
-
-
-
+# Load data
 data_path <- "~/Documents/Dropbox/Coursera/Exploratory_Data_Analysis_20140707/CP1/household_power_consumption.txt"
 # There is a header and the delimiter is a semicolon
 data      <- read.table(data_path, header = TRUE, sep = ";", stringsAsFactors = FALSE)
@@ -75,50 +38,15 @@ png(
   filename = "~/Documents/Dropbox/Coursera/Exploratory_Data_Analysis_20140707/CP1/plot1.png",
   width = 480,
   height = 480
-  )
+)
 # Some aesthetics include red bars, x axis label, and a main title
 hist(
   x = data$Global_active_power,
   col = "red",
   xlab = "Global Active Power (kilowatts)",
   main = "Global Active Power"
-  )
+)
 # Turn off PNG device to save to file
 dev.off()
 
 # DONE!
-
-
-
-
-
-
-##################
-# plot2 code
-##################
-
-
-
-
-
-
-
-
-##################
-# plot3 code
-##################
-
-
-
-
-
-
-
-##################
-# plot4 code
-##################
-
-
-
-
-

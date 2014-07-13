66edfaf8cb20a07857d6c9426d65f538a1767cc4
atet <atet@users.noreply.github.com>
2014-07-12 18:46:52 -0700

    Creating new directory ExData_Plotting2 for second course project

---
+++ ExData_Plotting2/main.R
@@ -0,0 +1,6 @@
+## AK 20140712
+## github: atet
+## NOTE: I am submitting this early, but seeing how this is a public repos, anyone can see it right away.
+## If you need help, you can use this as a guide to understanding the problem, but please do not just copy/paste this code.
+## R has a steep learning curve, but you will get it with time and experience, I have faith in you.
+#####################################################################################################
